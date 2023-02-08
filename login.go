31265e1ac289941050a020f065e527e53b8ba461
fbigabig <fbigabig@gmail.com>
2023-02-08 15:49:09 -0500

    encryption

--- login.go
+++ login.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -17,21 +19,17 @@ type User struct {
 }
 
 func authenticate(inName string, inPassword string, matchUser *User) bool { //authenticate password, this needs to be made secure, rn everything is plaintext
-	if matchUser.password == inPassword {
-		fmt.Println("success")
-		return true
-	}
-	fmt.Println("fail")
-	return false
+	err := bcrypt.CompareHashAndPassword([]byte(matchUser.password), []byte(inPassword))
+	return err == nil
 }
-func login(inName string, inPassword string, userlist *[]User) {
+func login(inName string, inPassword string, userlist *[]User) bool { //checks if the user exists, then calls authenticate
 	for _, v := range *userlist { //looks through the list of users
 
 		if v.username == inName {
-			authenticate(inName, inPassword, &v)
+			return authenticate(inName, inPassword, &v)
 		}
 	}
-	fmt.Println("done")
+	return false
 }
 func getInput(reader *bufio.Scanner) string { //simplifies getting input from the user
 	reader.Scan()
@@ -40,27 +38,41 @@ func getInput(reader *bufio.Scanner) string { //simplifies getting input from th
 	input = strings.TrimSuffix(input, "\n")
 	return input
 }
+<<<<<<< Updated upstream
 
 
 func main() {
 	reader := bufio.NewScanner(os.Stdin)
+=======
+func doLogIn(reader *bufio.Scanner, userList *[]User) bool {
+	fmt.Println("Username") //get user info
+	var username string = getInput(reader)
+	fmt.Println("Password")
+	var password string = getInput(reader)
+	return login(username, password, userList) //try to login with the info
+}
+func main() {
+	reader := bufio.NewScanner(os.Stdin)
+
+>>>>>>> Stashed changes
 	file, err := os.Open("userlist.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 	userList := make([]User, 0)
-
 	fileReader := bufio.NewScanner(file)
 	for fileReader.Scan() { //read in userlist
 		name := fileReader.Text()
 		fileReader.Scan()
 		password := fileReader.Text()
+		fmt.Println("UNPW: " + name + " " + password + "\n")
 		tempUser := new(User)
 		tempUser.username = name
 		tempUser.password = password
 		userList = append(userList, *tempUser)
 	}
+<<<<<<< Updated upstream
 	fmt.Println("Username") //get user info
 	var username string = getInput(reader)
 	fmt.Println("Password")
@@ -88,6 +100,65 @@ func isAuth(username, password string) bool {
 	}
 
 	return password == pass
+=======
+	err = file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var loggedIn bool = doLogIn(reader, &userList)
+	if loggedIn {
+		fmt.Println("Logged in")
+	} else {
+		for !loggedIn {
+			fmt.Println("Login failed")
+			fmt.Println("Type 1 to try again, or anything else to exit.")
+			var choice string = getInput(reader)
+			if choice == "1" {
+				loggedIn = doLogIn(reader, &userList)
+			} else {
+				fmt.Println("Goodbye!")
+				os.Exit(0)
+			}
+		}
+		fmt.Println("Logged in")
+	}
+	fmt.Println("Type 1 to add a user, or anything else to exit.")
+	var choice string = getInput(reader)
+	if choice == "1" {
+
+		file2, err := os.OpenFile("userlist.txt", os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file2.Close()
+		fmt.Println("Username") //get user info
+		var username string = getInput(reader)
+		//get user info
+		fmt.Println("Password") //get user info
+		var password string = getInput(reader)
+		_, err = file2.WriteString(username + "\n")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		hashedPW, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("test")
+		_, err = file2.WriteString(string(hashedPW) + "\n")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = file2.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Println("Goodbye!")
+		os.Exit(0)
+	}
+>>>>>>> Stashed changes
 }
 // checks if the user and pass match based on the map returns bool
 
