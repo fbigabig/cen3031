dd43ba78981373e594814d8533e3d5f3adf272d8
PabloBueno1 <pbueno@ufl.edu>
2023-02-08 15:40:50 -0500

    Update login.go

--- login.go
+++ login.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import (
 	"bufio"
 	"fmt"
@@ -39,9 +40,11 @@ func getInput(reader *bufio.Scanner) string { //simplifies getting input from th
 	input = strings.TrimSuffix(input, "\n")
 	return input
 }
+
+
 func main() {
 	reader := bufio.NewScanner(os.Stdin)
-	file, err := os.Open("C:\\Users\\aaaaaa\\go\\src\\github.com\\cen3031\\userlist.txt")
+	file, err := os.Open("userlist.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,4 +66,66 @@ func main() {
 	fmt.Println("Password")
 	var password string = getInput(reader)
 	login(username, password, &userList) //try to login with the info
+
 }
+*/
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var users = map[string]string{
+	"testUser": "Pass",
+}
+// map which holds the user names and pass words will be hashed on the client side
+
+func isAuth(username, password string) bool {
+	pass, isAuthorrized := users[username]
+	if !isAuthorrized {
+		return false
+	}
+
+	return password == pass
+}
+// checks if the user and pass match based on the map returns bool
+
+func startUp(web http.ResponseWriter, rep *http.Request) {
+	web.Header().Add("Content-Type", "application/json")
+	username, password, isIn := rep.BasicAuth()
+	// uses built in Basic Auth func to check if user is on the site
+
+	if !isIn {
+		web.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
+		web.WriteHeader(http.StatusUnauthorized)
+		web.Write([]byte(`{"message": "No User Was Given"}`))
+		return
+	}
+	// prompts user to enter credentials pop on the site also on terminal 
+
+	if !isAuth(username, password) {
+		web.Header().Add("WWW-Authenticate", `Basic realm="Give username and password"`)
+		web.WriteHeader(http.StatusUnauthorized)
+		web.Write([]byte(`{"message": "Incorrect Info"}`))
+		return
+	}
+	// if the user and pass do not match display message
+
+	web.WriteHeader(http.StatusOK)
+	web.Write([]byte(`{"message": "Log in Successful"}`))
+	// once logged in display welcome message
+
+	return
+}
+
+func main() {
+	http.HandleFunc("/", startUp)
+	fmt.Println("Starting Server at port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// creating a http server using go
+}
+
+// based on the code on https://umesh.dev/blog/how-to-implement-http-basic-auth-in-gogolang/
+
