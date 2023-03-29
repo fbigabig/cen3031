7d898431431e8f34efabafa8c32c71d10c4e78f0
Matthew Huynh <83797785+mattchooo@users.noreply.github.com>
2023-03-29 15:50:11 -0400

    Add files via upload
    search - contains search bar to find video games
    database - displays search results
    environment - contains link to SQL/Go database to be merged with database display in frontend
    interceptors/services - handles HTTP/database errors

---
+++ search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit{
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: '',
+      password: ''
+    })
+  };
+
+  submit(): void {
+    console.log(this.form.getRawValue());
+    //this.http.post('http://localhost:8080/create', this.form.getRawValue()).subscribe(()=>this.router.navigate(['/login']));
+    this.router.navigate(['/database']);
+
+  }
+}
